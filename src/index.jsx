6757451d03f0bf85c4e8becfcc82c6491e0f12d8
seonghyeon <hyeon@apartmentary.com>
2019-01-19 01:33:48 +0900

    add container style

--- src/index.jsx
+++ src/index.jsx
@@ -8,8 +8,10 @@ import Piano from '@/components/Piano';
 class App extends PureComponent {
   render() {
     return (
-      <div>
-        <Piano />
+      <div className="container">
+        <div className="piano-wrapper">
+          <Piano />
+        </div>
       </div>
     );
   }
